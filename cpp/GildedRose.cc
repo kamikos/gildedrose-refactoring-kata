3118e917031575201d433ce6544ab57c0e563289
Kamikos <kamil.orzechowsky@gmail.com>
2022-12-13 14:56:13 +0100

    predefined strings

--- cpp/GildedRose.cc
+++ cpp/GildedRose.cc
@@ -9,13 +9,18 @@ GildedRose::GildedRose(::std::vector<Item> && items) : items(::std::move(items))
 
 void GildedRose::updateQuality()
 {
+    std::string nameBackstage = "Backstage passes to a TAFKAL80ETC concert";
+    std::string nameAged = "Aged Brie";
+    std::string nameSulfuras = "Sulfuras, Hand of Ragnaros";
+
+
     for (int i = 0; i < items.size(); i++)
     {
-        if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert")
+        if (items[i].name != nameAged && items[i].name != nameBackstage)
         {
             if (items[i].quality > 0)
             {
-                if (items[i].name != "Sulfuras, Hand of Ragnaros")
+                if (items[i].name != nameSulfuras)
                 {
                     items[i].quality--;
                 }
@@ -27,7 +32,7 @@ void GildedRose::updateQuality()
             {
                 items[i].quality++;
 
-                if (items[i].name == "Backstage passes to a TAFKAL80ETC concert")
+                if (items[i].name == nameBackstage)
                 {
                     if (items[i].sellIn < 11)
                     {
@@ -48,20 +53,20 @@ void GildedRose::updateQuality()
             }
         }
 
-        if (items[i].name != "Sulfuras, Hand of Ragnaros")
+        if (items[i].name != nameSulfuras)
         {
             items[i].sellIn--;
         }
 
         if (items[i].sellIn < 0)
         {
-            if (items[i].name != "Aged Brie")
+            if (items[i].name != nameAged)
             {
-                if (items[i].name != "Backstage passes to a TAFKAL80ETC concert")
+                if (items[i].name != nameBackstage)
                 {
                     if (items[i].quality > 0)
                     {
-                        if (items[i].name != "Sulfuras, Hand of Ragnaros")
+                        if (items[i].name != nameSulfuras)
                         {
                             items[i].quality--;
                         }
